91f5a86192dad95c5512ff36bdc9e934b83e9297
Daniel Lopez <d@niel.to>
2024-07-31 09:14:40 -0700

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Main, Title } from '@/components/ui'
+
+export default function Home() {
+  return (
+    <Main className='flex flex-col p-4'>
+      <div className='flex flex-grow flex-col items-center justify-center space-y-4'>
+        <Title>wordlink</Title>
+      </div>
+    </Main>
+  )
+}
---
+++ src/components/ui/index.ts
@@ -0,0 +1,4 @@
+import Main from './main'
+import Title from './title'
+
+export { Main, Title }
